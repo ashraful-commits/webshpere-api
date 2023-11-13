37907aac9f5e712ff6f42241400515464f72316c
ashraful-commits <ashrfulalam10000@gmail.com>
2023-11-14 04:47:35 +0600

    updated

--- Controller/SellerController.js
+++ Controller/SellerController.js
@@ -255,15 +255,14 @@ try {
                 "30d"
               );
               res
-                .cookie("accessToken", Token, {
-                  httpOnly: true,
-                  secure: process.env.APP_ENV === "development" ? false : true,
-                  sameSite:"strict",
-                  domain: 'wordsphere.vercel.app', 
-                  path: '/',
-
-                  maxAge: 1000 * 60 * 60 * 24 * 7,
-                })
+              .cookie("accessToken", Token, {
+                httpOnly: true,
+                secure: process.env.APP_ENV !== "development", 
+                sameSite: "strict",
+                domain: 'wordsphere.vercel.app', 
+                path: '/',
+                maxAge: 1000 * 60 * 60 * 24 * 7,
+              })
                 .status(200)
                 .json({
                   token: Token,
